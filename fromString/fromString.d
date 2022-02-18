1059f5d5015e8d50e5ad307eb95dc171dbe7cb50
bradeythedev <maybp77@yahoo.com>
2022-02-18 18:14:00 +0800

    Create fromString

---
+++ fromString/fromString.d
@@ -0,0 +1,42 @@
+module fromString;
+import std.stdio;
+
+bool toBool(string input) {
+
+    return false;
+}
+
+byte toByte(string input) {
+
+    return 0;
+}
+
+ubyte toUByte(string input) {
+
+    return 0;
+}
+
+char toChar(string input) {
+
+    return ' ';
+}
+
+int toInt(string input) {
+
+    return 0;
+}
+
+uint toUInt(string input) {
+
+    return 0;
+}
+
+long toLong(string input) {
+
+    return 0;
+}
+
+ulong toULong(string input) {
+
+    return 0;
+}
